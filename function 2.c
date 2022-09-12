7b08e4dc9338cf9624c02855d8b9196892152ea7
Neeraj Kumar Kannoujiya <96954081+neeraj7388011@users.noreply.github.com>
2022-09-12 23:45:32 +0530

    Add files via upload

---
+++ function 2.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void goodMorning();
+void goodAfternoon();
+void goodNight();
+
+int main(){
+goodMorning ();
+goodAfternoon ();
+goodNight ();
+return 0;
+}
+void goodMorning (){
+printf("Good morning Neeraj\n");
+}
+
+void goodAfternoon (){
+printf("Good Afternoon Neeraj\n");
+}
+
+void goodNight (){
+printf("Good Night Neeraj\n");
+}
+
+
+
+
+
---
+++ function basic.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void display();
+
+int main(){
+int a;
+printf("Neeraj ");
+display();
+printf(" Kannoujiya ");
+return 0;
+}
+void display (){
+printf("kumar");
+}
+
+
+
